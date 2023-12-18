5b07687ed63b88088d7a6173ddbb9320219b4272
bhupendra <bhupendra.sisodia806@gmail.com>
2023-12-18 22:43:30 +0530

    Added CRUD apis and admin interface for blogs

--- personal_blog/api/admin.py
+++ personal_blog/api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import BLOGMODEL
+from django.utils import timezone
 
-# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'published_date']
+    list_filter = ['title']
+    search_fields = ['title', 'description']
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'description')
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not change:  # If it's a new entry
+            obj.published_date = timezone.now()
+        obj.save()
+
+
+admin.site.register(BLOGMODEL, BlogAdmin)
+
